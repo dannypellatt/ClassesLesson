f07e431913c614b90b63a104728f52c1e8d00465
dannypellatt <danielpellatt92@gmail.com>
2022-07-13 15:44:46 -0400

    initial commit

---
+++ ClassesLesson/Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ClassesLesson
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
+
---
+++ ClassesLesson/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClassesLesson;
+
+Car myCurrentCar = new Car();
+
+myCurrentCar.Make = "Mercedes";
+myCurrentCar.Model = "GL450";
+myCurrentCar.Year = 2015;
+
+Car myFirstCar = new Car();
+
+myFirstCar.Make = "Mini";
+myFirstCar.Model = "Cooper";
+myFirstCar.Year = 1967;
+
+Console.WriteLine($"My first car was a {myFirstCar.Make} " +
+    $"{myFirstCar.Model} made in the year {myFirstCar.Year}");
+
+Console.WriteLine($"My current car is a {myCurrentCar.Make} " +
+    $"{myCurrentCar.Model} made in the year {myCurrentCar.Year}.");
